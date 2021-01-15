1ae28c654b74943a1406ad700c1b26f09501536f
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-15 15:24:32 +0900

    [Add] バリデーション

---
+++ app/models/job_offer.rb
@@ -0,0 +1,11 @@
+class JobOffer < ApplicationRecord
+  # association
+  belongs_to :organization
+
+  # validation
+  with_options presence: true do
+    validates :title
+    validates :url, uniqueness: true
+    validates :event_date
+  end  
+end
