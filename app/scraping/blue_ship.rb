74d5323561196aedb5eb7e457835a17b7713939b
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-17 00:53:00 +0900

    [Add] スクレイピング無限ループ回避

--- app/scraping/blue_ship.rb
+++ app/scraping/blue_ship.rb
@@ -1,5 +1,5 @@
 class BlueShip
-  attr_reader :driver, :wait, :url, :data
+  attr_reader :driver, :wait, :url, :is_next_page, :data
 
   def initialize
     # seleniumを初期化
@@ -10,6 +10,8 @@ class BlueShip
     @wait = Selenium::WebDriver::Wait.new(timeout: 6)
 
     @url = "https://blueshipjapan.com/search/event/catalog?area=0&amp;text_date=&amp;date=1&amp;text_keyword=&amp;cancelled=0&amp;cancelled=1&amp;order=desc"
+    @is_next_page = true
+    @loop_num = 0
     @data = []
   end
 
@@ -25,7 +27,7 @@ class BlueShip
     begin
       scraping_data = []
 
-      while (true)
+      while (is_next_page)
         wait.until { driver.find_element(:xpath, "//*[@id='search_result']/div/div/ul/li").displayed? }
 
         scraping_data += driver.find_elements(:xpath, "//*[@id='search_result']/div/div/ul/li")
@@ -33,17 +35,20 @@ class BlueShip
 
         next_page_link = driver.find_element(:xpath, "//*[@id='search_result']/div/div/div/span/following-sibling::a")
         next_page_link.click
+
+        # 万が一、ページ構成が変わった時の無限ループ回避
+        avoid_loop_20
       end
 
     rescue Selenium::WebDriver::Error::NoSuchElementError
       # 最後のページまでスクレイピングした時
       puts "スクレイピング成功"
+      return scraping_data
     rescue => error
       p error
     end
 
     driver.quit
-    return scraping_data
   end
 
   def parse_element(element)
@@ -54,5 +59,10 @@ class BlueShip
       job_offer_url:     element.find_element(:xpath, ".//div[1]/h2/a").attribute('href'),
       event_date:        element.find_element(:xpath, ".//a/p/span").text
     }
+  end
+
+  def avoid_loop_20
+    @loop_num += 1 
+    @is_next_page = false if @loop_num >= 20
   end
 end
--- app/scraping/moshicom.rb
+++ app/scraping/moshicom.rb
@@ -1,5 +1,5 @@
 class Moshicom
-  attr_reader :driver, :wait, :url, :data
+  attr_reader :driver, :wait, :url, :is_next_page, :data
 
   def initialize
     # seleniumを初期化
@@ -10,6 +10,8 @@ class Moshicom
     @wait = Selenium::WebDriver::Wait.new(timeout: 6)
 
     @url = "https://moshicom.com/search/?s=1&keyword=&event_start_date=&event_end_date=&entry_status=yes&except_eventup=no&scale=0&day_entry=no&measurement=no&user_id=0&search_type=0&recommend_event=true&recommend_course=true&recommend_facility=true&mode=1&l=100&o=0&m=1&keywords=&amp%3Bdate_start=&amp%3Bdate_end=&amp%3Bdisciplines%5B%5D=11&amp%3Bscale=0&amp%3Bsort=2&amp%3Bdisp_limit=20&amp%3Bmode=1"
+    @is_next_page = true
+    @loop_num = 0
     @data = []
   end
 
@@ -25,7 +27,7 @@ class Moshicom
     begin
       scraping_data = []
 
-      while (true)
+      while (is_next_page)
         wait.until { driver.find_element(:xpath, "//*[@id='event_search']/div[3]/div[1]/section[2]/div").displayed? }
 
         scraping_data += driver.find_elements(:xpath, "//*[@id='event_search']/div[3]/div[1]/section[2]/div")
@@ -33,18 +35,22 @@ class Moshicom
 
         next_page_link = driver.find_element(:xpath, "//*[@id='event_search']/div[3]/div[1]/section[2]/nav/ul/li[17]/a")
         next_page_link.click
+
+        # 万が一、ページ構成が変わった時の無限ループ回避
+        avoid_loop_20
       end
 
     rescue Selenium::WebDriver::Error::ElementClickInterceptedError
       # 最後のページまでスクレイピングした時
       puts "スクレイピング成功"
+      return scraping_data
     rescue => error
       # TODO 最後のページまでスクレイピングできてない
       p error
+      return scraping_data
     end
 
     driver.quit
-    return scraping_data
   end
 
   def parse_element(element)
@@ -55,5 +61,10 @@ class Moshicom
       job_offer_url:     element.find_element(:xpath, ".//h3/a").attribute('href'),
       event_date:        element.find_element(:xpath, ".//div[1]/div[1]/time").text
     }
+  end
+
+  def avoid_loop_20
+    @loop_num += 1 
+    @is_next_page = false if @loop_num >= 20
   end
 end
