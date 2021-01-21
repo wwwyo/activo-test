cbcd0b5037f3709fd2fa4646adfe6f32c590604d
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-21 13:09:02 +0900

    [Add] organizationモデル

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -8,4 +8,15 @@ class Organization < ApplicationRecord
     validates :url, uniqueness: true
   end
   validates :is_checked, inclusion: {in: [true, false]}
+
+  def iterate_find_or_create(data_hash_lists)
+    data_hash_lists.each do |list|
+      Organization.find_or_create_by(list)
+    end
+  end
+
+  def find_id_by_url(url)
+    organization = Organization.find_by(url: url)
+    organization.id
+  end
 end
