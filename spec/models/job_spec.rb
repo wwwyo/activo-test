2230b09a0cf47295b6266b138c014a5f9ee710aa
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-21 13:05:09 +0900

    [Add] モデルの単体テスト

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before do
+      @job = FactoryBot.build(:job)
+    end
+    it '求人が重複して登録されない' do
+      @job.save
+      job_not_unique = FactoryBot.build(:job)
+      job_not_unique.url = @job.url
+      job_not_unique.valid?
+      expect(job_not_unique.errors.full_messages).to include('Url has already been taken')
+    end
+
+    it '団体に紐づいてない求人は登録されない' do
+      job = Job.new(title: "求人", url: "https://kyuuzin.co.jp", event_date: "2020-1-23")
+      job.valid?
+      expect(job.errors.full_messages).to include('Organization must exist')
+    end
+  end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before do
+      @organization = FactoryBot.build(:organization)
+    end
+    it '団体が重複して登録されない' do
+      @organization.save
+      organization_not_unique = FactoryBot.build(:organization)
+      organization_not_unique.url = @organization.url
+      organization_not_unique.valid?
+      expect(organization_not_unique.errors.full_messages).to include('Url has already been taken')
+    end
+  end
 end
