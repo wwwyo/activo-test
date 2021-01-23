c2b94ef4ca047fc8fb4a424ee20326e772e0d9ad
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-23 11:21:38 +0900

    [Add] cron rbenv初期化

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, "#{Rails.root}/log/cron.log"
+env :PATH, ENV['PATH']
 
 rails_env = ENV['RAILS_ENV'] || :development
 # cronを実行する環境変数をセット
 set :environment, rails_env
+# rbenvを初期化
+set :job_template, "/bin/zsh -l -c ':job'"
+job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 every 1.days, at: '7:00 am' do
   begin
