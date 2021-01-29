71aad9a2547d73fe1710d4e6cceab95e15f88324
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-29 21:59:24 +0900

    [Add] model testの練習

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+  it "has a valid factory" do
+    expect(build(:organization)).to be_valid
+  end
+
   describe 'validation' do
     before do
-      @organization = FactoryBot.build(:organization)
+      @organization = build(:organization)
     end
-    it '団体が重複して登録されない' do
+    it 'forbid to have not unique url' do
       @organization.save
-      organization_not_unique = FactoryBot.build(:organization)
-      organization_not_unique.url = @organization.url
+      organization_not_unique = FactoryBot.build(:organization, url: @organization.url)
       organization_not_unique.valid?
       expect(organization_not_unique.errors.full_messages).to include('Url has already been taken')
     end
